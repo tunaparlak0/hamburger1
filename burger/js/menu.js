ddc693c30fa302bf83d75b0eb4686d24225671cb
tunaparlak0 <tuna.parlak@ogr.sakarya.edu.tr>
2024-05-12 22:17:26 +0300

    bootstrapsiz
    final hali

--- burger/js/menu.js
+++ burger/js/menu.js
@@ -1,13 +1,9 @@
-
-// Sepete eklenen öğeleri tutmak için bir dizi 
-let cartItems = [];
-
-// Sepetin toplam tutarını takip etmek için bir değişken 
+ 
+let cartItems = []; 
 let total = 0;
 
 // Sepete öğe ekleme fonksiyonu
 function addToCart(name, price) {
-  // Eğer aynı ürün zaten sepete eklenmişse
   const existingItem = cartItems.find(item => item.name === name);
   if (existingItem) {
     existingItem.quantity++;
@@ -15,36 +11,26 @@ function addToCart(name, price) {
     // Yeni bir öğe olarak sepete ekle
     cartItems.push({ name: name, price: price, quantity: 1 });
   }
-
   // Toplam tutarı güncelle
   total += price;
-
-  // Sepet listesini güncelle
   updateCart();
 }
-
 // Sepet listesini güncelleyen fonksiyon
 function updateCart() {
   const cartElement = document.getElementById("cart-items");
   const totalElement = document.getElementById("total");
-
   // Önceki sepeti temizle
   cartElement.innerHTML = "";
-
-  // Her bir öğe için listeye bir öğe ekle
   cartItems.forEach(item => {
     const li = document.createElement("li");
     li.textContent = `${item.name} x ${item.quantity}: $${item.price * item.quantity}`;
     cartElement.appendChild(li);
   });
-
   // Toplam tutarı güncelle
   totalElement.textContent = `Total: $${total.toFixed(2)}`;
 }
-
 // Sayfa yüklendiğinde çağrılacak fonksiyon
 window.onload = function() {
-  // Tüm Add to cart düğmelerine tıklama olayı ekle
   const addToCartButtons = document.querySelectorAll('.box-bottom .btn');
   addToCartButtons.forEach(button => {
     button.addEventListener('click', function(event) {
@@ -56,6 +42,7 @@ window.onload = function() {
     });
   });
 };
+//Sayfanın aşşağısına atmak için buton
 document.getElementById("scrollToBottomBtn").addEventListener("click", function() {
   window.scrollTo({
     top: document.body.scrollHeight,
